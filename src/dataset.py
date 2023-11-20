47d4f6b1bdab0e59a6c7b7ef34a435ef66edec4c
Meredith Dunn <astromarinadunn@gmail.com>
2023-11-20 15:18:56 -0600

    Consolidate and add comments to dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,196 +1,156 @@
-
 import os
 import numpy as np
 import torch
 import random
 import cv2
 
-class EuropaIceBlockMetaset(torch.utils.data.Dataset):
-    # Use in analysis after model building, provides metadata on each observation
-    def __init__(self, dataset_type, root, img_dir, lbl_dir, transforms=None):
-        self.dataset_type = dataset_type
+# Use directly in model training
+class EuropaIceBlockDataset(torch.utils.data.Dataset):
+    """
+    Creates a PyTorch dataset from the torch.utils.data.Dataset class,
+    applies optional transforms.
+    """
+
+    def __init__(self, root, img_dir, lbl_dir, config, transforms=None):
+        """
+        Initializes variables.
+
+        Parameters:
+            root (str):
+                Root directory for data.
+            img_dir (str):
+                Directory for images.
+            lbl_dir (str):
+                Directory for ground truth mask PNGs.
+            transforms (Optional[torchvision.transforms.Compose]):
+                Optional transforms to apply to images and masks.
+        """
         self.root = root
         self.transforms = transforms
         self.img_dir = img_dir
         self.lbl_dir = lbl_dir
-        img_dir_files = list(sorted(os.listdir(os.path.join(root, img_dir))))
-        lbl_dir_files = list(sorted(os.listdir(os.path.join(root, lbl_dir))))
+        self.config = config
+        img_dir_files = sorted(os.listdir(os.path.join(root, img_dir)))
+        lbl_dir_files = sorted(os.listdir(os.path.join(root, lbl_dir)))
         self.imgs = self.get_npy_files(img_dir_files)
         self.masks = self.get_npy_files(lbl_dir_files)
+
     def get_npy_files(self, files):
-        npy_list = []
-        for file in files:
-            split_file = file.split(".")
-            ext = split_file[-1]
-            if ext == "npy":
-                npy_list.append(file)
-        return npy_list
+        return [file for file in files if file.endswith(".npy")]
+
     def get_crop_metadata(self, file, img_shape):
-        file_name = file.split('.')[0]
+        file_name = os.path.splitext(file)[0]
         split_file = file_name.split('_')
-        alias = split_file[0]
+        region_alias = split_file[0]
         x = int(split_file[1])
         y = int(split_file[2])
         h = img_shape[0]
         w = img_shape[1]
-        return (alias, [x, y, w, h])
-    def __len__(self):
-        return len(self.imgs)
+        y_min = int(split_file[2])
+        y_max = y_min + h
+        x_min = int(split_file[1])
+        x_max = x_min + w
+        return (region_alias, (y_min, x_min, y_max, x_max))
+
     def __getitem__(self, idx):
-        img = np.load(os.path.join(self.root, self.img_dir, self.imgs[idx]))
-        mask = np.load(os.path.join(self.root, self.lbl_dir, self.masks[idx]))
+        """
+        Forms a PyTorch dataset. Inherits lists of filepaths for images and
+        labels, and optional transforms. For each image and label, opens them
+        with PIL, converts to numpy array. Unique objects instances are detected,
+        and masks are split into binary masks. Bounding boxes are then calculated
+        for each object instance in the mask, and box areas are calculated.
+        Finally, each image and label are converted to PyTorch tensors, and
+        optional transforms are applied if specified.
+
+        Parameters
+            idx (int):
+                Index of image and label to be processed.
+
+        Returns:
+            img (torch.Tensor):
+                PyTorch tensor of image with shape [channels, H, W]
+
+            target (Dict[str, torch.Tensor]):
+                A dict containing the following fields:
+                    - boxes (FloatTensor[N, 4]): the coordinates of the N
+                        bounding boxes of [x0, y0, x1, y1] format, ranging
+                        from 0 to W and 0 to H
+                    - labels (Int64Tensor[N]): the label for each bounding box
+                    - image_id (Int64Tensor[1]): an image identifier. Should
+                        be unique between all images in dataset, and is used
+                        during evaluation.
+                    - area (Tensor[N]): The area of the bounding box.
+                    - iscrowd (UInt8Tensor[N]): instances with iscrowd=True
+                        will be ignored during evaluation.
+                    - masks (UInt8Tensor[N, H, W]): The segmentation masks
+                        for each one of the objects.
+
+            crop_metadata (Tuple[str, List[int]]):
+                A tuple containing the following fields:
+                    - region_alias (str): the alias of the region
+                    - [x, y, w, h] (List[int]): the coordinates of the
+                        bounding box of the region
+        """
+
+        # open each image, mask numpy file
+        img = np.load(os.path.join(self.root, self.img_dir, self.imgs[idx]), allow_pickle=True)
+        mask = np.load(os.path.join(self.root, self.lbl_dir, self.masks[idx]), allow_pickle=True)
         crop_metadata = self.get_crop_metadata(self.imgs[idx], img.shape)
-        obj_ids = np.unique(mask)[1:] 
-        masks = (mask == obj_ids[:, None,None]) # This code always confused me with the syntax
+
+        # instances are encoded as different colors, with 0 being background
+        obj_ids = np.unique(mask)[1:]  # exclude background
+
+        # split the color-encoded mask into a set of binary masks
+        masks = mask == obj_ids[:, None, None]  # shape is [num_instances, H, W]
+
+        # get bounding box coordinates for each mask
         boxes = []
-        for i in range(len(obj_ids)): # I think Ahmed wanted better logic here?
-          pos = np.where(masks[i]) 
-          boxes.append([np.min(pos[1]), np.min(pos[0]), np.max(pos[1]), np.max(pos[0])])   
-        #come back to this with tensor versus numpy array
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        for i in range(len(obj_ids)):
+            pos = np.where(masks[i].nonzero())
+            xmin, xmax = np.min(pos[1]), np.max(pos[1])
+            ymin, ymax = np.min(pos[0]), np.max(pos[0])
+
+            # coords of bounding boxes in [x1, y1, x2, y2] format
+            if xmin < xmax and ymin < ymax:
+                boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        if len(boxes) > 0:
+            # shape is [num_instances, [x1, y1, x2, y2]]
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        else:
+            boxes = torch.zeros((0, 4), dtype=torch.float32)
+
+        # label for each ground-truth bounding box; there is only one class
+        labels = torch.ones(len(obj_ids), dtype=torch.int64)
+
+        # segmentation binary masks for each instance [num_instances, H, W]
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        # unique integer image identifier, used during evaluation
+        image_id = torch.tensor([idx])
+
+        # area of the bounding box: (y2 - y1) * (x2 - x1)
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        target = dict([("boxes", boxes),
-                       ("area", area),
-                       ("labels", torch.ones((len(obj_ids),), dtype=torch.int64) ),
-                       ("masks", torch.as_tensor(masks, dtype=torch.uint8)), 
-                       ("image_id", torch.tensor([idx])), 
-                       ("iscrowd", torch.zeros((len(obj_ids),), dtype=torch.int64))])
-        #import pdb; pdb.set_trace()
-        if self.transforms is not None: # double check this??
+
+        # Crowd box in COCO is a bounding box around several instances. Thus,
+        # suppose all instances are not crowd. Instances with iscrowd=True
+        # will be ignored during evaluation
+        iscrowd = torch.zeros(len(obj_ids), dtype=torch.int64)
+
+        target = {
+            "boxes": boxes,
+            "labels": labels,
+            "masks": masks,
+            "image_id": image_id,
+            "area": area,
+            "iscrowd": iscrowd
+        }
+
+        if self.transforms is not None:
             img, target = self.transforms(img, target)
-        #import pdb; pdb.set_trace()
+
         return img, target, crop_metadata
 
-class EuropaIceBlockDataset(torch.utils.data.Dataset):
-    def __init__(self,root, img_dir, lbl_dir, transforms=None):
-        self.root = root
-        self.transforms = transforms
-        self.img_dir = img_dir
-        self.lbl_dir = lbl_dir
-        img_dir_files = list(sorted(os.listdir(os.path.join(root, img_dir))))
-        lbl_dir_files = list(sorted(os.listdir(os.path.join(root, lbl_dir))))
-        self.imgs = self.get_npy_files(img_dir_files)
-        self.masks = self.get_npy_files(lbl_dir_files)
-    def get_npy_files(self, files):
-        npy_list = []
-        for file in files:
-            split_file = file.split(".")
-            ext = split_file[-1]
-            if ext == "npy":
-                npy_list.append(file)
-        return npy_list
-    def get_crop_metadata(self, file, img_shape):
-        file_name = file.split('.')[0]
-        split_file = file_name.split('_')
-        alias = split_file[0]
-        x = int(split_file[1])
-        y = int(split_file[2])
-        h = img_shape[0]
-        w = img_shape[1]
-        return (alias, [x, y, w, h])
     def __len__(self):
         return len(self.imgs)
-    def __getitem__(self, idx):
-        img = np.load(os.path.join(self.root, self.img_dir, self.imgs[idx]))
-        mask = np.load(os.path.join(self.root, self.lbl_dir, self.masks[idx]))
-        crop_metadata = self.get_crop_metadata(self.imgs[idx], img.shape)
-        obj_ids = np.unique(mask)[1:] 
-        masks = (mask == obj_ids[:, None,None]) # This code always confused me with the syntax
-        boxes = []
-        for i in range(len(obj_ids)): # I think Ahmed wanted better logic here?
-          pos = np.where(masks[i]) 
-          boxes.append([np.min(pos[1]), np.min(pos[0]), np.max(pos[1]), np.max(pos[0])])   
-        #come back to this with tensor versus numpy array
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        target = dict([("boxes", boxes),
-                       ("area", area),
-                       ("labels", torch.ones((len(obj_ids),), dtype=torch.int64) ),
-                       ("masks", torch.as_tensor(masks, dtype=torch.uint8)), 
-                       ("image_id", torch.tensor([idx])), 
-                       ("iscrowd", torch.zeros((len(obj_ids),), dtype=torch.int64))])
-        #import pdb; pdb.set_trace()
-        if self.transforms is not None: # double check this??
-            img, target = self.transforms(img, target)
-        #import pdb; pdb.set_trace()
-        return img, target
-
-class EuropaIceBlockDatasetTorch(torch.utils.data.Dataset):
-    # Use directly in model training
-    def __init__(self, root, img_dir, lbl_dir, transforms=None):
-        self.root = root
-        self.transforms = transforms
-        self.img_dir = img_dir
-        self.lbl_dir = lbl_dir
-        img_dir_files = list(sorted(os.listdir(os.path.join(root, img_dir))))
-        lbl_dir_files = list(sorted(os.listdir(os.path.join(root, lbl_dir))))
-        self.imgs = self.get_npy_files(img_dir_files)
-        self.masks = self.get_npy_files(lbl_dir_files)
-    def get_npy_files(self, files):
-        npy_list = []
-        for file in files:
-            split_file = file.split(".")
-            ext = split_file[-1]
-            if ext == "npy":
-                npy_list.append(file)
-        return npy_list
-    def get_crop_metadata(self, file, img_shape):
-        file_name = file.split('.')[0]
-        split_file = file_name.split('_')
-        alias = split_file[0]
-        x = int(split_file[1])
-        y = int(split_file[2])
-        h = img_shape[0]
-        w = img_shape[1]
-        return (alias, [x, y, w, h])
-    def __len__(self):
-        return len(self.imgs)
-    def __getitem__(self, idx):
-        img = np.load(os.path.join(self.root, self.img_dir, self.imgs[idx]))
-        mask = np.load(os.path.join(self.root, self.lbl_dir, self.masks[idx]))
-        crop_metadata = self.get_crop_metadata(self.imgs[idx], img.shape)
-        obj_ids = np.unique(mask)[1:] 
-        masks_og = (mask == obj_ids[:, None,None]) # This code always confused me with the syntax
-        #come back to this with tensor versus numpy array
-
-        boxes = []
-        while len(boxes) == 0:
-            # import pdb; pdb.set_trace()
-            seed = np.random.randint(2147482647)
-            random.seed(seed)
-            torch.manual_seed(seed)
-            if self.transforms is not None: # double check this??
-                transformed_img = self.transforms(img)
-
-            random.seed(seed)
-            torch.manual_seed(seed)
-            if self.transforms is not None: # double check this??
-                transformed_masks = self.transforms(masks_og)
-
-
-            np_masks = transformed_masks.squeeze().numpy().swapaxes(0,1)
-            for i in range(len(obj_ids)): # I think Ahmed wanted better logic here?
-                pos = np.where(np_masks[i]) 
-                if (pos[0].shape[0] == 0) or (pos[1].shape[0] == 0): # check if valid label even exists
-                    continue
-                box = [np.min(pos[1]), np.min(pos[0]), np.max(pos[1]), np.max(pos[0])]
-                box_area = (box[2] - box[0])*(box[3] - box[1])
-                if box_area > 50:
-                    boxes.append(box) #xmin, ymin, xmax, ymax  
-
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-
-        target = dict([("boxes", boxes),
-                       ("area", area),
-                       ("labels", torch.ones((len(obj_ids),), dtype=torch.int64) ),
-                       ("masks", transformed_masks), 
-                       ("image_id", torch.tensor([idx])), 
-                       ("iscrowd", torch.zeros((len(obj_ids),), dtype=torch.int64))])
-
-
-        
-        """ If youre using the notebook code the below code is how u transform"""
-        # if self.transforms is not None: # double check this??
-        #     img, target = self.transforms(img, target)
-        return transformed_img, target
