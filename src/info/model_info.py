5c5d410df7a3ce0684f6d69f7f5ee8d91ed996d5
Meredith Dunn <astromarinadunn@gmail.com>
2023-11-20 14:48:42 -0600

    consolidate info into src/config.py

--- src/info/model_info.py
+++
@@ -1,3 +0,0 @@
-TRANSFER_TRAINED_MODEL_FOLDER = "model_weights"
-MAIN_MODEL_PATH = "model_weights/type_1_transfer_trained_model.pth" # Need to think of abstraction
-CUMULATIVE_TEST_MODEL_PATH = "model_weights/cumulative_test_model.pth"
