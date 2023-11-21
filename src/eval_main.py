096417752b4495ffd0745ac5cfc14e21ba3002fe
Meredith Dunn <astromarinadunn@gmail.com>
2023-11-21 17:49:47 -0600

    Add comments

--- src/eval_main.py
+++ src/eval_main.py
@@ -2,6 +2,6 @@ from test_objs.test_cumulative_eval import TestCumulativeEvaluator
 
 if __name__ == "__main__":
 
-    test_obj_cumulative = TestCumulativeEvaluator()
-    test_obj_cumulative.temp_save()
-    test_obj_cumulative.run_tests()
+    test_obj_cumulative = TestCumulativeEvaluator()  # create the test object
+    test_obj_cumulative.temp_save()  # save the results to a temp file
+    test_obj_cumulative.run_tests()  # run the tests
