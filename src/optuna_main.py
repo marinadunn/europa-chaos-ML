df4b7f1d8033e8f20276d2a04915be4ba377b802
Meredith Dunn <astromarinadunn@gmail.com>
2023-11-21 17:14:21 -0600

    Add comments

--- src/optuna_main.py
+++ src/optuna_main.py
@@ -1,11 +1,13 @@
 from optuna_wrapper import OptunaWrapper
-from utils.optuna_utility.objectives import objective, avg_f1_objective, avg_recall_objective
+from src.utils.optuna_utility.objectives import objective, avg_f1_objective, avg_recall_objective
 
 def optuna_search():
-    optuna_obj = OptunaWrapper("recall", 10, avg_recall_objective)
+    optuna_obj = OptunaWrapper("recall", 10, avg_recall_objective)  # 10 trials, recall as metric
     optuna_obj.optimize()
     optuna_obj.save_best_params()
+    optuna_obj.plot_search()
 
 
 if __name__ == "__main__":
+    # perform search using wrapper, save best params, plot search results
     optuna_search()
