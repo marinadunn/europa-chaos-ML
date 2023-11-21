4b77809ef7d95a6e260bf67eda2a4df0985d3314
Meredith Dunn <astromarinadunn@gmail.com>
2023-11-21 17:47:40 -0600

    Change to all blocks, not just plates; add comments

--- src/data_info_script.py
+++ src/data_info_script.py
@@ -8,14 +8,10 @@ from config import (CHAOS_REGION_ALIAS_TO_FILE_MAP,
                     )
 from utils.file_utils import create_output_csv, append_input_to_file
 
-# Avg plate size km
-# std plate size km
-# Total Plate Area km
-# Total Chaos Area km
 
 def calculate_metrics(region_alias):
     """
-    Calculate plate metrics for a given region alias.
+    Calculate ice block size metrics (in km) for a given chaos region.
 
     Args:
         region_alias (str): Alias of the region.
@@ -23,10 +19,13 @@ def calculate_metrics(region_alias):
     Returns:
         str: Computed metrics for the region.
     """
+    # Get resolution for region
     res = CHAOS_REGION_RESOLUTION_MAP[region_alias] / 1000
 
-    # Chaos Region Area
+    # Load Chaos Region mask
     chaos_region_mask = cv2.imread(CHAOS_REGION_ALIAS_TO_REGION_MAP[region_alias])[:, :, 0]
+
+    # Calculate Chaos Region Area
     chaos_region_area = np.sum(np.where(chaos_region_mask > 0, 1, 0)) * res * res
 
     # Object Count
@@ -35,32 +34,38 @@ def calculate_metrics(region_alias):
     struc_data = chaos_region_lbls.view(rgb_type)
     unique_colors = np.unique(struc_data)
 
-    # Plate Area Stats
-    plate_count = 0
-    plate_areas = []
+    # Calculate Ice Block Areas and Counts
+    block_count = 0
+    block_areas = []
 
     for void_color in unique_colors:
         mask_color = void_color.tolist()
+        # Skip black mask
         if mask_color == (0, 0, 0):
             continue
         single_mask = np.all(chaos_region_lbls == mask_color, axis=2)
         crop_area = np.sum(np.where(single_mask > 0, 1, 0))
+
+        # Only count ice blocks with area > 5 pixels
         if crop_area > 5:
             crop_area = crop_area * res * res
-            plate_areas.append(crop_area)
-            plate_count += 1
+            block_areas.append(crop_area)
+            block_count += 1
 
-    plate_areas = np.asarray(plate_areas)
-    total_plate_area = np.sum(plate_areas)
-    mean_plate_area = np.mean(plate_areas)
-    std_plate_area = np.std(plate_areas)
+    # Calculate Ice Block Area Metrics
+    block_areas = np.asarray(block_areas)
+    total_block_area = np.sum(block_areas)
+    mean_block_area = np.mean(block_areas)
+    std_block_area = np.std(block_areas)
 
-    plate_coverage = total_plate_area / chaos_region_area * 100
-    obs = f"{region_alias} & {plate_count} & {chaos_region_area:.3f} & {total_plate_area:.3f} & {mean_plate_area:.3f} & {std_plate_area:.3f} & {plate_coverage:.2f}%"
+    # Calculate Ice Block Coverage
+    block_coverage = total_block_area / chaos_region_area * 100
+    obs = f"{region_alias} & {block_count} & {chaos_region_area:.3f} & {total_block_area:.3f} & {mean_block_area:.3f} & {std_block_area:.3f} & {block_coverage:.2f}%"
+
     return obs
 
 
-if __name__ == "__main__":
+def main():
     """Main function to calculate and write plate metrics for different regions."""
     data_path = f"{INFO_OUTPUT_PATH}/pixel_metrics.txt"
     header = f"Region & Plate Count & Chaos Area & Plate Area & Plate Area Mean & Plate Area Std & Plate Coverage\n"
@@ -76,6 +81,7 @@ if __name__ == "__main__":
     for region_alias in region_aliases:
         obs = calculate_metrics(region_alias)
         append_input_to_file(data_path, obs)
+
 
 if __name__ == "__main__":
     main()
