b2b385429c4c96fcf688f4ef76ff8e785bd4a587
Meredith Dunn <astromarinadunn@gmail.com>
2023-11-20 19:06:55 -0600

    Added docstrings

--- src/optuna_wrapper.py
+++ src/optuna_wrapper.py
@@ -4,41 +4,70 @@ from config import OPTUNA_OUTPUT_PATH
 
 
 class OptunaWrapper():
-    # Functions as a simple wrapper for optuna
+    """
+    A simple wrapper for Optuna to streamline optimization tasks.
+
+    Attributes:
+        MAXIMAL_METRICS (list): List of metrics to maximize during optimization.
+        MINIMAL_METRICS (list): List of metrics to minimize during optimization.
+        PARAM_SAVE_FILE (str): Filename for saving the best parameters.
+
+    Methods:
+        __init__: Initializes the OptunaWrapper.
+        optimize: Runs the optimization process.
+        get_best_trial: Returns the best trial.
+        get_best_params: Returns the best parameters.
+        get_best_score: Returns the best score.
+        save_best_params: Saves the best parameters to a file.
+    """
     MAXIMAL_METRICS = ["precision", "recall", "iou", "f1"]
     MINIMAL_METRICS = ["loss"]
     PARAM_SAVE_FILE = "optuna_best_params.txt"
 
     def __init__(self, optim_metric, trial_count, obj_fn):
+        """
+        Initialize the OptunaWrapper.
+
+        Args:
+            optim_metric (str): Metric to optimize.
+            trial_count (int): Number of trials in the optimization.
+            obj_fn (callable): Objective function for optimization.
+        """
         make_dir(OPTUNA_OUTPUT_PATH)
         self.optim_metric = optim_metric
         self.study = None
         self.obj_fn = obj_fn
         self.trial_count = trial_count
         self.optimized = False
+
         if optim_metric in OptunaWrapper.MAXIMAL_METRICS:
             self.study = optuna.create_study(direction="maximize")
         elif optim_metric in OptunaWrapper.MINIMAL_METRICS:
             self.study = optuna.create_study(direction="minimize")
         else:
-            print("Optunawrapper does not support this metric yet, may not be using the right approach for given metric")
+            print("OptunaWrapper does not support this metric yet. Consider a different approach.")
 
     def optimize(self):
+        """Run the optimization process."""
         self.study.optimize(self.obj_fn, self.trial_count)
         self.optimized=True
 
     def get_best_trial(self):
+        """Return the best trial."""
         return self.study.best_trial
 
     def get_best_params(self):
+        """Return the best parameters."""
         best_trial = self.study.best_trial
         return best_trial.params
 
     def get_best_score(self):
+        """Return the best score."""
         best_trial = self.study.best_trial
         return best_trial.value
 
     def save_best_params(self):
+        """Save the best parameters to a file."""
         save_file_path = f"{OPTUNA_OUTPUT_PATH}/{OptunaWrapper.PARAM_SAVE_FILE}"
         clear_file(save_file_path)
 
