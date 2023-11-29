c7a8dfd00ba77d59bab3582d3e6c5f4d9fc753ac
Meredith Dunn <astromarinadunn@gmail.com>
2023-11-29 17:23:42 -0600

    Fix imports, change default variables to best

--- src/cross_val.py
+++ src/cross_val.py
@@ -1,41 +1,47 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
 
-from src.config import (TRANSFER_TRAINED_MODEL_FOLDER,
-                        CV_OUTPUT_PATH,
-                        CHAOS_REGION_ALIAS_TO_FILE_MAP,
-                        CHAOS_REGION_ALIAS_TO_LABEL_MAP,
-                        CHAOS_REGION_ALIAS_TO_REGION_MAP
-                        )
-from src.model_objects.rcnn_model import MaskRCNN
-from src.model_objects.trainable_maskrcnn import TrainableMaskRCNN
-from src.utils.optuna_utility.evaluation import MaskRCNNCumulativeOutputEvaluator
-from src.utils.file_utils import make_dir, clear_and_remake_directory, append_input_to_file
+from config import (TRANSFER_TRAINED_MODEL_FOLDER,
+                    CV_OUTPUT_PATH,
+                    CHAOS_REGION_ALIAS_TO_FILE_MAP,
+                    CHAOS_REGION_ALIAS_TO_LABEL_MAP,
+                    CHAOS_REGION_ALIAS_TO_REGION_MAP
+                    )
+from model_objects.rcnn_model import MaskRCNN
+from model_objects.trainable_maskrcnn import TrainableMaskRCNN
+from utils.optuna_utility.evaluation import MaskRCNNCumulativeOutputEvaluator
+from utils.file_utils import make_dir, clear_and_remake_directory, append_input_to_file, create_output_csv
 
 # Create output directories and files
-clear_and_remake_directory(CV_OUTPUT_PATH)
+#clear_and_remake_directory(CV_OUTPUT_PATH)
 csv_path = f"{CV_OUTPUT_PATH}/pixel_metrics.csv"
 header = f"chaos_region,crop_size,f1,precision,recall,best_threshold\n"
-file_utils.create_output_csv(csv_path, header)
+create_output_csv(csv_path, header)
 
 # Constants
-small_crop_size = 175
+small_crop_size = 250
 stride = 64
 stride_eval = stride
-mini_thresh = -2
+min_thresh = 0.1
 region_expand = 64
 num_classes = 2
 arch_id = 2  # Mask RCNN version 2
-num_epochs = 13
+num_epochs = 15
 
 def cross_val_pixel_csv():
     """
     Perform cross-validation and save pixel metrics to a CSV file.
     """
-    regions = ["Co", "A", "ii", "B",
-               "C", "D", "ee", "F",
-               "G", "hh", "I", "jj", "kk"]
+    regions = [ "A", "B", "C", "D",
+               "E", "F", "G", "H", "I",
+               "aa", "bb", "Co", "dd",
+               "ee", "ff", "gg", "hh",
+               "ii", "jj", "kk"
+               ]
 
     start = 0
     end = len(regions)
@@ -55,19 +61,23 @@ def cross_val_pixel_csv():
         model = train_obj.get_model()
         model_obj = MaskRCNN()
         model_obj.set_model(model)
+        # Evaluate model
         test_output(model_obj, test_region)
 
 def crop_image(img, region_activation, region_expand):
+    # Get crop bounds
     x_min = np.min(region_activation[1]) - region_expand
     x_max = np.max(region_activation[1]) + region_expand
     y_min = np.min(region_activation[0]) - region_expand
     y_max = np.max(region_activation[0]) + region_expand
+
     return img[y_min:y_max, x_min:x_max].copy()
 
 def test_output(model_obj, region):
     """
     Evaluate model output on a specific region and save metrics and visualizations.
     """
+    # Define evaluator
     eval_obj = MaskRCNNCumulativeOutputEvaluator()
 
     # Load images
@@ -100,7 +110,7 @@ def test_output(model_obj, region):
     best_thresh = eval_obj.get_best_thresh_f1(lbl_crop,
                                               pred_logit_dist,
                                               thresh_count=100,
-                                              thresh_min=mini_thresh
+                                              thresh_min=min_thresh
                                               )
 
     # Calculate metrics
@@ -125,7 +135,7 @@ def test_output(model_obj, region):
     auc, fprs, tprs = eval_obj.calc_pixel_auc(lbl_crop,
                                               pred_logit_dist,
                                               thresh_count=100,
-                                              thresh_min=mini_thresh
+                                              thresh_min=min_thresh
                                               )
     plt.plot(fprs, tprs)
     plt.title(f"Chaos {region} Pixel ROC Curve")
