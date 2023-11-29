5ba7da7cd174bc65ffe7f5a0f3321229215deca9
Meredith Dunn <astromarinadunn@gmail.com>
2023-11-29 16:42:55 -0600

    Modify for non-CUDA device

--- src/utils/engine.py
+++ src/utils/engine.py
@@ -14,55 +14,90 @@ import config as config
 
 
 def train_one_epoch(model, optimizer, data_loader, epoch, print_freq, device=config.device):
-    model.train()
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    header = 'Epoch: [{}]'.format(epoch)
+    """
+    Train the model for one epoch
+    """
+    # Training with CUDA capable GPU
+    if device == 'cuda':
+        model.train()
+        metric_logger = utils.MetricLogger(delimiter="  ")
+        metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+        header = 'Epoch: [{}]'.format(epoch)
 
-    lr_scheduler = None
-    if epoch == 0:
-        warmup_factor = 1. / 1000
-        warmup_iters = min(1000, len(data_loader) - 1)
+        lr_scheduler = None
 
-        lr_scheduler = utils.warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor)
+        if epoch == 0:
+            warmup_factor = 1. / 1000
+            warmup_iters = min(1000, len(data_loader) - 1)
 
-    for images, targets in metric_logger.log_every(data_loader, print_freq, header):
-        images = list(image.to(device) for image in images)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            lr_scheduler = utils.warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor)
 
-        loss_dict = model(images, targets)
-        losses = sum(loss for loss in loss_dict.values())
+        for images, targets in metric_logger.log_every(data_loader, print_freq, header):
+            images = list(image.to(device) for image in images)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        # reduce losses over all GPUs (if available) for logging purposes
-        if device == 'cuda':
+            loss_dict = model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+
+            # reduce losses over all GPUs (if available) for logging purposes
             loss_dict_reduced = utils.reduce_dict(loss_dict)
             losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
             loss_value = losses_reduced.item()
-        else:
+
+            if not math.isfinite(loss_value):
+                print("Loss is {}, stopping training".format(loss_value))
+                print(loss_dict_reduced)
+                sys.exit(1)
+
+            optimizer.zero_grad()
+            losses.backward()
+            optimizer.step()
+
+            if lr_scheduler is not None:
+                lr_scheduler.step()
+
+            metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+            metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+
+        return metric_logger
+
+    # Training with CPU
+    else:
+        model.train()
+        print('Epoch: [{}]'.format(epoch))
+
+        lr_scheduler = None
+
+        for images, targets in enumerate(data_loader):
+            images = list(image.to(device) for image in images)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+            loss_dict = model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+
             loss_value = losses.item()
 
-        if not math.isfinite(loss_value):
-            print("Loss is {}, stopping training".format(loss_value))
-            print(loss_dict_reduced)
-            sys.exit(1)
+            if not math.isfinite(loss_value):
+                print("Loss is {}, stopping training".format(loss_value))
+                print(loss_dict)
+                sys.exit(1)
 
-        optimizer.zero_grad()
-        losses.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            losses.backward()
+            optimizer.step()
 
-        if lr_scheduler is not None:
-            lr_scheduler.step()
+            if lr_scheduler is not None:
+                lr_scheduler.step()
 
-        if device == 'cuda':
-            metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
-        else:
-            metric_logger.update(loss=losses, **loss_dict)
-        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-
-    return metric_logger
+            print(f'Losses: {losses}, {loss_dict}')
+            print(f'Learning rate: {optimizer.param_groups[0]["lr"]}')
 
 
 def _get_iou_types(model):
+    """
+    Get the evaluation metrics for the model
+    """
     model_without_ddp = model
 
     if isinstance(model, torch.nn.parallel.DistributedDataParallel):
@@ -79,6 +114,9 @@ def _get_iou_types(model):
 
 @torch.no_grad()
 def evaluate(model, data_loader, device):
+    """
+    Evaluate the model on the validation set
+    """
     n_threads = torch.get_num_threads()
     torch.set_num_threads(1)
     model.eval()
